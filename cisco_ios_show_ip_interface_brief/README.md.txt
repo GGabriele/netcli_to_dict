2a63c4351026faebbfe4826486d2188d201151fc
Gabriele Gerbino <gabrielegerbino@gmail.com>
2015-09-16 11:32:40 +0200

    First commit

---
+++ cisco_ios_show_ip_interface_brief/README.md.txt
@@ -0,0 +1,16 @@
+{
+  "interfaces": {
+    "FastEthernet0/0": {
+      "ip address": "172.16.2.2", 
+      "method": "NVRAM", 
+      "protocol": "up", 
+      "status": "up"
+    }, 
+    "FastEthernet1/0": {
+      "ip address": "172.16.4.1", 
+      "method": "NVRAM", 
+      "protocol": "up", 
+      "status": "up"
+    }
+  }
+}
---
+++ cisco_ios_show_ip_interface_brief/show_ip_interface_brief.py
@@ -0,0 +1,25 @@
+import json
+import re
+
+with open('show_ip_interface_brief.txt', 'r') as f:
+    node_diagram = {}
+    results = f.read()
+    interface_split = results.split('\n')
+    prova = interface_split[2::]
+    interface_info = {}
+    for line in prova:
+        if len(line) > 5:
+            interface = {}
+            splitted_line = line.split()
+            interface['ip address'] = splitted_line[1]
+            interface['method'] = splitted_line[3]
+            interface['status'] = splitted_line[4]
+            if len(splitted_line) > 6:
+                interface['status'] = splitted_line[4] + ' ' + splitted_line[5]
+                interface['protocol'] = splitted_line[6]
+            else:
+                interface['protocol'] = splitted_line[5]
+                interface_info[splitted_line[0]] = interface
+                node_diagram["interfaces"] = interface_info
+
+print(json.dumps(node_diagram, sort_keys=True, indent=2))
